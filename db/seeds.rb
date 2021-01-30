c5f046c9fdd3d86c422d96cd6e8ecf1f23081182
Chris Bellairs <c.m.bellairs@gmail.com>
2021-01-30 16:54:36 +0000

    Backend framework completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialised = open(url).read
+ingredients_parsed = JSON.parse(ingredients_serialised)
+
+puts 'Clearing Ingredient DB'
+Ingredient.destroy_all
+puts 'DB cleared'
+
+ingredients_parsed['drinks'].each do |ingredient|
+  new_ingredient = Ingredient.create!(name: ingredient['strIngredient1'])
+  puts "#{new_ingredient.name} created"
+end
+
+puts 'Seeding completed!'
